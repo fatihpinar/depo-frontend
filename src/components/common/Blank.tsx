cf19ed8ef0c3d2f4e1481372af7b30e7385e847a
Fatih Pınar <fatihpinar@outlook.com>
2025-12-02 02:19:06 +0300

    Backend: deploy-ready config

--- src/components/common/Blank.tsx
+++ src/components/common/Blank.tsx
@@ -1,5 +1,5 @@
-import PageBreadcrumb from "../components/common/PageBreadCrumb";
-import PageMeta from "../components/common/PageMeta";
+import PageBreadcrumb from "./PageBreadCrumb";
+import PageMeta from "./PageMeta";
 
 export default function Blank() {
   return (
